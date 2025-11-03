676dd178a324e4d3530a9c2719f3da2045990081
Shevchenko013 <shevchenko.lacerda@gmail.com>
2025-11-03 19:25:03 -0300

    Diversas aulas e exercícios do Python

---
+++ funções/maior_elemento.py
@@ -0,0 +1,3 @@
+def maior_elemento(lista):
+    return max(lista)
+print(maior_elemento([2,3,5,7,11,13])) 
---
+++ funções/nascimento_idade.py
@@ -0,0 +1,4 @@
+def calcular_ano_nascimento(idade, ano_atual):
+    return ano_atual - idade
+print(calcular_ano_nascimento(15, 2025)) 
+    
